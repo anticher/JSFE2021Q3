d9bc21f2e94652acdad9299f6bdfaaf1e94e371d
Aliaksei Ramashka <anticherq@gmail.com>
2021-10-03 17:02:33 +0300

    feat: add player js funcionality

--- museum/js/player.js
+++ museum/js/player.js
@@ -1,79 +1,112 @@
 const progress = document.querySelector('#progressBar');
 const volume = document.querySelector('#volumeBar');
+const bigPlayBtn = document.querySelector('.video__big-play');
 const playPauseBtn = document.querySelector('#playPauseBtn')
 const mute = document.querySelector('#mute')
 const muteAdaptive = document.querySelector('#muteAdaptive')
-const fullScreen = document.querySelector('#fullScreen')
+const fullScreenBtn = document.querySelector('#fullScreen')
 const player = document.querySelector('#player')
+const playerContainer = document.querySelector('.video__player')
+const playerControls = document.querySelector('.video__controls')
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === ' ') {
+    handlePlayButton()
+  } else if (event.key === 'm' || event.key === 'M') {
+    onMuteClick()
+  } else if (event.key === 'f' || event.key === 'F') {
+    toggleFullscreen()
+  } else if (event.shiftKey && event.key == ',' || event.key == '<') {
+    console.log(player.playbackRate)
+  } else if (event.shiftKey && event.key == '.' || event.key == '>') {
+    player.playbackRate -= 0.5
+  }
+})
+
 
 //progress
 progress.value = 54
 progress.style.background = `linear-gradient(to right, #710707 0%, #710707 ${progress.value}%, #C4C4C4 ${progress.value}%, #C4C4C4 100%)`
 
 player.ontimeupdate = () => {
-    console.log(player.currentTime)
-    progress.value = 100 / player.duration * player.currentTime
-    progress.style.background = `linear-gradient(to right, #710707, #710707 ${progress.value}%, #C4C4C4 ${progress.value}%, #C4C4C4 100%)`
+  if (player.duration === player.currentTime) {
+    player.pause();
+    playPauseBtn.classList.remove('playing');
+    bigPlayBtn.classList.add('video__big-play-active')
+  }
+  progress.value = 100 / player.duration * player.currentTime
+  progress.style.background = `linear-gradient(to right, #710707, #710707 ${progress.value}%, #C4C4C4 ${progress.value}%, #C4C4C4 100%)`
 }
 
-progress.addEventListener('input', function() {
-    const value = this.value;
-    player.currentTime = value / (100 / player.duration)
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+progress.addEventListener('input', function () {
+  const value = this.value;
+  player.currentTime = value / (100 / player.duration)
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
 
 })
 
 //mute
-mute.value = 1
+let muteValue = 0
 
-mute.addEventListener('click', function() {
-    if (this.classList.contains('muted')) {
-        this.classList.remove('muted')
-        player.volume = mute.value
-    } else {
-        this.classList.add('muted')
-        mute.value = player.volume
-        player.volume = 0
-    }
-})
-
-muteAdaptive.addEventListener('click', function() {
-  if (this.classList.contains('muted')) {
-      this.classList.remove('muted')
-      player.volume = mute.value
+function onMuteClick() {
+  if (mute.classList.contains('muted')) {
+    mute.classList.remove('muted')
+    volume.value = muteValue
+    volume.style.background = `linear-gradient(to right, #710707 0%, #710707 ${muteValue}%, #C4C4C4 ${muteValue}%, #C4C4C4 100%)`
+    player.volume = muteValue / 100
   } else {
-      this.classList.add('muted')
-      mute.value = player.volume
-      player.volume = 0
+    mute.classList.add('muted')
+    muteValue = volume.value
+    volume.value = 0
+    volume.style.background = `linear-gradient(to right, #710707 0%, #710707 ${volume.value}%, #C4C4C4 ${volume.value}%, #C4C4C4 100%)`
+    player.volume = 0
   }
-})
+}
+
+
+mute.addEventListener('click', onMuteClick)
+
+muteAdaptive.addEventListener('click', onMuteClick)
 
 
 //volume
 player.volume = volume.value / 100
-// player.volume.addEventListener('change', function() {
+// player.addEventListener('change', function() {
 //     volume.value = player.volume * 100
 // })
 
-volume.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
-    player.volume = value / 100
-    mute.value = player.volume
+function onVolumeChanged() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+  player.volume = value / 100
+  muteValue = value
+  if (value > 0) {
     mute.classList.remove('muted')
-})
+  } else {
+    mute.classList.add('muted')
+  }
+}
+
+volume.addEventListener('input', onVolumeChanged)
 
 
 //playPause
-playPauseBtn.addEventListener("click", handlePlayButton, false);
-playVideo();
+playPauseBtn.addEventListener("click", handlePlayButton);
+
+
+player.addEventListener('click', handlePlayButton)
+
+bigPlayBtn.addEventListener('click', handlePlayButton)
 
 async function playVideo() {
   try {
     await player.play();
-    playPauseBtn.classList.add("playing");
-  } catch(err) {
-    playPauseBtn.classList.remove("playing");
+    playPauseBtn.classList.add('playing');
+    bigPlayBtn.classList.remove('video__big-play-active')
+
+  } catch (err) {
+    playPauseBtn.classList.remove('playing');
+    bigPlayBtn.classList.add('video__big-play-active')
   }
 }
 
@@ -82,22 +115,29 @@ function handlePlayButton() {
     playVideo();
   } else {
     player.pause();
-    playPauseBtn.classList.remove("playing");
+    playPauseBtn.classList.remove('playing');
+    bigPlayBtn.classList.add('video__big-play-active')
   }
 }
 
 //fullScreen
 
-fullScreen.addEventListener('click', toggleFullscreen)
+fullScreenBtn.addEventListener('click', toggleFullscreen)
 
 function toggleFullscreen() {
-  
-    if (!document.fullscreenElement) {
-      player.requestFullscreen().catch(err => {
-        alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-      });
-    } else {
-      document.exitFullscreen();
+  if (document.fullscreen) {
+    document.exitFullscreen();
+    playerControls.classList.remove('video__controls-absolute')
+    fullScreenBtn.classList.remove('video__full-screen-active')
+  } else {
+    playerControls.classList.add('video__controls-absolute')
+    fullScreenBtn.classList.add('video__full-screen-active')
+    if (playerContainer.requestFullscreen) {
+      playerContainer.requestFullscreen()
+    } else if (playerContainer.mozRequestFullScreen) {
+      playerContainer.mozRequestFullScreen()
+    } else if (playerContainer.webkitRequestFullscreen) {
+      playerContainer.webkitRequestFullscreen()
     }
   }
-
+}
