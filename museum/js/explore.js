34773a0d75b9e8b5f5cc040d2ea0108a79eab20a
Aliaksei Ramashka <anticherq@gmail.com>
2021-10-08 21:28:27 +0300

    feat: add explore slider

--- museum/js/explore.js
+++ museum/js/explore.js
@@ -1,62 +1,58 @@
 
-// I hope this over-commenting helps. Let's do this!
-// Let's use the 'active' variable to let us know when we're using it
-let active = false;
+let active = false
 
-// First we'll have to set up our event listeners
-// We want to watch for clicks on our scroller
-document.querySelector('.explore__slider-scroller').addEventListener('mousedown',function(){
-  active = true;
-  // Add our scrolling class so the scroller has full opacity while active
-  document.querySelector('.explore__slider-scroller').classList.add('scrolling');
-});
-// We also want to watch the body for changes to the state,
-// like moving around and releasing the click
-// so let's set up our event listeners
-document.body.addEventListener('mouseup',function(){
-  active = false;
-  document.querySelector('.explore__slider-scroller').classList.remove('scrolling');
-});
-document.body.addEventListener('mouseleave',function(){
-  active = false;
-  document.querySelector('.explore__slider-scroller').classList.remove('scrolling');
-});
 
-// Let's figure out where their mouse is at
-document.body.addEventListener('mousemove',function(e){
-  if (!active) return;
-  // Their mouse is here...
-  let x = e.pageX;
-  // but we want it relative to our wrapper
-  x -= document.querySelector('.explore__slider-wrapper').getBoundingClientRect().left;
-  // Okay let's change our state
-  scrollIt(x);
-});
+document.querySelector('.explore__slider-scroller').addEventListener('mousedown', function () {
+  active = true
 
-// Let's use this function
-function scrollIt(x){
-    let transform = Math.max(0,(Math.min(x,document.querySelector('.explore__slider-wrapper').offsetWidth)));
-    document.querySelector('.explore__slider-after').style.width = transform+"px";
-    document.querySelector('.explore__slider-scroller').style.left = transform-25+"px";
+  document.querySelector('.explore__slider-scroller').classList.add('scrolling')
+})
+
+document.body.addEventListener('mouseup', function () {
+  active = false
+  document.querySelector('.explore__slider-scroller').classList.remove('scrolling')
+})
+document.body.addEventListener('mouseleave', function () {
+  active = false
+  document.querySelector('.explore__slider-scroller').classList.remove('scrolling')
+})
+
+document.body.addEventListener('mousemove', function (e) {
+  if (!active) return
+  let x = e.pageX
+  x -= document.querySelector('.explore__slider-wrapper').getBoundingClientRect().left
+  scrollIt(x)
+})
+
+
+function scrollIt(x) {
+  let transform = Math.max(0, (Math.min(x, document.querySelector('.explore__slider-wrapper').offsetWidth)))
+  document.querySelector('.explore__slider-after').style.width = transform + "px"
+  document.querySelector('.explore__slider-scroller').style.left = transform - 25 + "px"
 }
 
-// Let's set our opening state based off the width, 
-// we want to show a bit of both images so the user can see what's going on
-scrollIt(150);
+//start position of slider
+scrollIt(document.querySelector('.explore__slider-wrapper').offsetWidth * 0.6)
 
-// And finally let's repeat the process for touch events
-// first our middle scroller...
-document.querySelector('.explore__slider-scroller').addEventListener('touchstart',function(){
-  active = true;
-  document.querySelector('.explore__slider-scroller').classList.add('scrolling');
-});
-document.body.addEventListener('touchend',function(){
-  active = false;
-  document.querySelector('.explore__slider-scroller').classList.remove('scrolling');
-});
-document.body.addEventListener('touchcancel',function(){
-  active = false;
-  document.querySelector('.explore__slider-scroller').classList.remove('scrolling');
-});
 
-document.body.addEventListener('touchmove',function(e){ if (!active) return; let x = e.touches[0].pageX; x -= document.querySelector('.explore__slider-wrapper').getBoundingClientRect().left; scrollIt(x); });
+document.querySelector('.explore__slider-scroller').addEventListener('touchstart', function () {
+  active = true
+  document.querySelector('.explore__slider-scroller').classList.add('scrolling')
+})
+document.body.addEventListener('touchend', function () {
+  active = false
+  document.querySelector('.explore__slider-scroller').classList.remove('scrolling')
+})
+document.body.addEventListener('touchcancel', function () {
+  active = false
+  document.querySelector('.explore__slider-scroller').classList.remove('scrolling')
+})
+
+document.body.addEventListener('touchmove', function (e) {
+  if (!active) {
+    return
+  }
+  let x = e.touches[0].pageX
+  x -= document.querySelector('.explore__slider-wrapper').getBoundingClientRect().left
+  scrollIt(x)
+})
